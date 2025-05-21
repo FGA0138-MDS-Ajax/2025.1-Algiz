efe18e306e6702d9ce717fcf979c082aaf240e86
TiagoSTdeLyra <56367136+TiagoSTdeLyra@users.noreply.github.com>
2025-05-21 19:52:01 -0300

    correções do login
    ✅ Alterações Realizadas:
    - imagem de fundo adicionada
    - formulário centralizado
    - logo no formulário é funcional

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,49 +12,58 @@ function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-hscreen bg-gray-100 pt-14">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h2 className="text-2x1 font-bold mb-6 text-center text-green-600">Login</h2>
-        <form onSubmit={handleLogin} className="space-y-4">
-          <div>
-            <label className="block mb-1 font-medium text-gray-700">Email</label>
-            <input
+    <div className="min-h-screen flex items-center justify-center bg-[url('/bg.png)] bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: "url('/bg.png')"}}>
+      <div className="absolute inset-0 bg-green-950/80 z-0"></div>
+      <form onSubmit={handleLogin} className="relative z-10 bg-white/10 backdrop-blur-md rounded-2xl border border-white/30 shadow-2xl p-8 w-[400px] flex flex-col items-center">
+
+        <Link to="/">
+          <img src="/logo.png" alt="Logo" className="w-24 mx-auto mb-4 cursor-pointer" />
+        </Link>
+        <h2 className="text-2xl font-bold mb-6 text-white text-center">Login</h2>
+
+        <div className="w-full mb-6">
+          <label className="block mb-1 text-white font-medium">Email</label>
+          <input
             type="email"
-            className="w-full border border-gray-300 rounded px-3 py-2"
+            className="input w-full"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label className="block mb-1 font-medium text-gray-700">Senha</label>
+
+        <div className="w-full mb-6">
+          <label className="block mb-1 text-white font-medium">Senha</label>
           <input
             type="password"
-            className="w-full border-gray-300 rounded px-3 py-2"
+            className="input w-full"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
             required
           />
         </div>
-        <div className="text-right">
+
+        <div className="w-full text-right mb-4">
           <Link to="/esqueci-senha" className="text-sm text-blue-600 hover:underline">
             Esqueci minha senha
           </Link>
         </div>
+
         <button
           type="submit"
           className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700 transition"
         >
           Login
         </button>
-      </form>
-      <p className="mt-4 text-center text-sm">
+
+      
+      <p className="text-center text-sm text-white">
         Não tem uma conta{' '}
-        <Link to="/cadastro" className="text-blue-600 hover:underline">
+        <Link to="/cadastro" className="text-blue-300 hover:underline">
           Cadastre-se
         </Link>
       </p>
-    </div>
+    </form>
   </div>
   );
 }
