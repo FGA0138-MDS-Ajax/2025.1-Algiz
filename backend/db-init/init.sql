d5bcda4c4616b817b86e6bbcba9720b3e6a2247e
Discicle <joaovsses@gmail.com>
2025-05-23 14:26:30 -0300

    :card_file_box: Criação dos arquivos do banco de dados

--- backend/db-init/init.sql
+++ backend/db-init/init.sql
@@ -1,11 +1,39 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
-    -- other columns as needed
-);
+-- Arquivo de inicialização e criação do banco de dados e suas tabelas
+--
+-- Comando de criação do banco de dados
+-- Executar apenas uma vez
+CREATE DATABASE IF NOT EXISTS EcoNet_DB;
 
--- Insert sample data if the table is empty
-INSERT INTO users (name, email)
-SELECT 'John Doe', 'john.doe@example.com'
-WHERE NOT EXISTS (SELECT 1 FROM users);
+-- Comando de criação das tabelas
+CREATE TABLE IF NOT EXISTS USUARIO (
+    idUsuario       INT         NOT NULL    AUTO_INCREMENT,
+    emailUsuario    VARCHAR(255)NOT NULL    UNIQUE,
+    telefoneUsuario INT         NOT NULL,
+    estado          VARCHAR(255)NOT NULL,
+    PRIMARY KEY (idUsuario)
+)ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS FISICO (
+    cpfFisico       INT(11)         NOT NULL,
+    nomeFisico      VARCHAR(255)    NOT NULL,
+    sobrenomeFisico VARCHAR(255)    NOT NULL,
+    sexo            CHAR(1)   NOT NULL,
+    dtNascimento    DATE            NOT NULL,
+    idUsuario       INT             NOT NULL,
+    PRIMARY KEY (cpfFisico),
+    FOREIGN KEY (idUsuario) REFERENCES USUARIO(idUsuario)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS JURIDICO (
+    cnpjJuridico    VARCHAR(14) NOT NULL,
+    razaoSocial     VARCHAR(255)NOT NULL,
+    nomeComercial   VARCHAR(255)NOT NULL    UNIQUE,
+    areaAtuacao     VARCHAR(255)NOT NULL,
+    idUsuario       INT         NOT NULL,
+    PRIMARY KEY (cnpjJuridico),
+    FOREIGN KEY (idUsuario) REFERENCES USUARIO(idUsuario)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)ENGINE=INNODB;
