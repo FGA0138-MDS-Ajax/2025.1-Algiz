bd49b2b817cf1899ad0aa6733091f9e0edd95cf8
marianagonzaga0 <marianarsg0605@gmail.com>
2025-05-25 15:59:07 -0300

    integrar rota de login no servidor

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', authRoutes);
 
 const PORT = 3001;
 
